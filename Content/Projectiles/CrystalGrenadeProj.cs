b376f047697c400072ec915f03d3d1abc5d4c6d2
SuperAndyHero <31840036+SuperAndyHero@users.noreply.github.com>
2022-08-08 18:29:01 -0500

    fixed grenade damage

--- Content/Projectiles/CrystalGrenadeProj.cs
+++ Content/Projectiles/CrystalGrenadeProj.cs
@@ -1,6 +1,7 @@
 using Microsoft.Xna.Framework;
 using Terraria;
 using Terraria.Audio;
+using Terraria.DataStructures;
 using Terraria.ID;
 using Terraria.ModLoader;
 
@@ -14,17 +15,30 @@ namespace CrystiliumMod.Content.Projectiles
 		{
 			DisplayName.SetDefault("Crystal Grenade");
 		}
-
+		private int oldDamage = 0;
 		public override void SetDefaults()
 		{
 			Projectile.CloneDefaults(ProjectileID.Grenade);
 			Projectile.timeLeft = 120;
 			Projectile.width = 26;
-			Projectile.height = 28;
+            Projectile.height = 28;
 		}
 
-		public override void Kill(int timeLeft)
+        public override void OnSpawn(IEntitySource source)
+        {
+			oldDamage = Projectile.damage;
+		}
+
+        public override bool PreAI()
+        {
+			Projectile.damage = oldDamage;
+			return true;
+        }
+
+        public override void Kill(int timeLeft)
 		{
+			Projectile.hostile = true;
+			Projectile.friendly = true;
 			SoundEngine.PlaySound(SoundID.Item27, Projectile.position);
 			for (int h = 0; h < 25; h++)
 			{
@@ -32,10 +46,11 @@ namespace CrystiliumMod.Content.Projectiles
 				float rand = Main.rand.NextFloat() * 6.283f;
 				vel = vel.RotatedBy(rand);
 				vel *= 5f;
-				int proj = Projectile.NewProjectile(Projectile.GetSource_Death(), Projectile.Center.X, Projectile.Center.Y + 20, vel.X, vel.Y, Mod.Find<ModProjectile>("Shatter" + (1 + Main.rand.Next(0, 3))).Type, (int)(Projectile.damage * .425), 0, Main.myPlayer);
+				int proj = Projectile.NewProjectile(Projectile.GetSource_Death(), Projectile.Center.X, Projectile.Center.Y + 20, vel.X, vel.Y, Mod.Find<ModProjectile>("Shatter" + (1 + Main.rand.Next(0, 3))).Type, (int)(oldDamage * .425), 0, Main.myPlayer);
 				Main.projectile[proj].friendly = true;
 				Main.projectile[proj].hostile = true;
 			}
+			Main.NewText(oldDamage);
 		}
 	}
 }
